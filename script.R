926a06c466cdaf2f530b527531b5c7a800ac9187
BalduinLandolt <balduin.landolt@unibas.ch>
2019-05-14 13:49:38 +0200

    Add: some more tree algorithms

--- script.R
+++ script.R
@@ -47,8 +47,12 @@ phy = phyDat(d, type = "USER", levels = c("?","-","0","1","2","3","4","5"))
 str(phy)
 summary(phy)
 
-# calculate tree from data
+# calculate trees from data
+# -------------------------
+# Distance Matrix
 tree = dist.ml(phy)
+
+# Neighbour Join
 nj_data = NJ(tree)
 
 # plot tree (in several different looks)
@@ -58,11 +62,27 @@ plot.phylo(nj_data, type = "unrooted", lab4ut = "axial")
 plot.phylo(nj_data, type = "unrooted")
 plot.phylo(nj_data, type = "radial")
 
-# safe tree to file
+
+# max pars
+pars_data = pratchet(phy)
+
+# plot tree (in several different looks)
+plot.phylo(pars_data, use.edge.length=FALSE, cex=0.75)
+plot.phylo(pars_data, use.edge.length=TRUE)
+plot.phylo(pars_data, type = "unrooted", lab4ut = "axial")
+plot.phylo(pars_data, type = "unrooted")
+plot.phylo(pars_data, type = "radial")
+
+
+bt = bootstrap.phyDat(phy,FUN = function(x)nj(dist.hamming(x)), bs=50)
+#consensus
+cnt = consensusNet(bt)
+plot(cnt)
+
+
+
+# safe tree to files
 write.tree(nj_data, file="data/trivial_tree.tre")
-
-
-
 
 # -> old notes... might still be of use
 # library(anchors)
@@ -111,6 +131,28 @@ plot.phylo(nj_data, use.edge.length=TRUE)
 plot.phylo(nj_data, type = "unrooted", lab4ut = "axial")
 plot.phylo(nj_data, type = "unrooted")
 plot.phylo(nj_data, type = "radial")
+
+
+
+
+
+# max pars
+pars_data = pratchet(phy)
+
+# plot tree (in several different looks)
+plot.phylo(pars_data, use.edge.length=FALSE, cex=0.75)
+plot.phylo(pars_data, use.edge.length=TRUE)
+plot.phylo(pars_data, type = "unrooted", lab4ut = "axial")
+plot.phylo(pars_data, type = "unrooted")
+plot.phylo(pars_data, type = "radial")
+
+
+bt = bootstrap.phyDat(phy,FUN = function(x)nj(dist.hamming(x)), bs=50)
+#consensus
+cnt = consensusNet(bt)
+plot(cnt, "2D")
+
+
 
 # safe tree to file
 write.tree(nj_data, file="data/random_tree.tre")
